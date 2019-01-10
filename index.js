1a97ef5c7ebf8f73bf6058afddee82724a2647b4
Matthew Carter <m@ahungry.com>
2019-01-10 16:03:50 -0500

    Calling http from scheme

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 // hello.js
 const fs = require('fs')
+const https = require('https');
 const addon = require('./build/Release/addon')
+
+function make_http () {
+  https.get('https://httpbin.org/ip', (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      const json = JSON.parse(data)
+      console.log(json)
+      addon.set_register(data)
+      // cb(data)
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+}
 
 console.log(addon.hello())
 // Prints: 'world'
@@ -13,7 +36,8 @@ const scm = buf.toString('ascii')
 addon.cb((msg) => console.log('Callback message was: ', msg))
 
 //console.log(addon.eval("(number->string (+ (my-sum 100 100) (+ 1 2 3 4 5)))"))
-console.log(addon.reg((x) => console.log("I was called from scheme: ", x)))
+//console.log(addon.reg((x) => console.log("I was called from scheme: ", x)))
+console.log(addon.reg(make_http))
 
 
 console.log("Setting register from node: ", addon.set_register("REGISTER-TEST"))
